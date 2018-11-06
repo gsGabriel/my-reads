db5ef1a7fa8992d0ada6ece211b8ba4c28f5ad03
gsGabriel <gs.gabrielcfa@outlook.com>
2018-11-05 22:12:55 -0200

    create tests for list books

---
+++ src/__tests__/ListBooks.test.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { mount } from '../enzyme';
+
+import ListBooks from '../components/ListBooks';
+
+describe('<ListBooks/>', () => {
+  test('renders list books without books', () => {
+    const books = [];
+    const shelfs = [
+      { key: 'currentlyReading', text: 'Current Reading' },
+      { key: 'wantToRead', text: 'Want to Read' },
+      { key: 'read', text: 'Read' }
+    ];
+    const updateBook = jest.fn();
+
+    const wrapper = mount(
+      <ListBooks books={books} shelfs={shelfs} onUpdateBook={updateBook} />
+    );
+
+    // Expect the wrapper div for a shelf to be defined
+    expect(wrapper.find('h3')).toBeDefined();
+
+    // Expect the wrapper property is the same a mock
+    expect(wrapper.prop('books')).toEqual(books);
+    expect(wrapper.prop('shelfs')).toEqual(shelfs);
+    expect(wrapper.prop('onUpdateBook')).toEqual(updateBook);
+  });
+
+  test('renders list books with books', () => {
+    const books = [
+      {
+        id: 1,
+        authors: ['Gabriel'],
+        imageLinks: {
+          thumbnail:
+            'http://books.google.com/books/content?id=sJf1vQAACAAJ&printsec=frontcover&img=1&zoom=0&source=gbs_api'
+        }
+      }
+    ];
+    const shelfs = [
+      { key: 'currentlyReading', text: 'Current Reading' },
+      { key: 'wantToRead', text: 'Want to Read' },
+      { key: 'read', text: 'Read' }
+    ];
+    const updateBook = jest.fn();
+
+    const wrapper = mount(
+      <ListBooks books={books} shelfs={shelfs} onUpdateBook={updateBook} />
+    );
+
+    // Expect the wrapper div for a shelf to be defined
+    expect(wrapper.find('h3')).toBeDefined();
+
+    // Expect the wrapper property is the same a mock
+    expect(wrapper.prop('books')).toEqual(books);
+    expect(wrapper.prop('shelfs')).toEqual(shelfs);
+    expect(wrapper.prop('onUpdateBook')).toEqual(updateBook);
+  });
+});
